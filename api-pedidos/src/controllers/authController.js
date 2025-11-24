ec3bcb6bc8ae82982132fb0577adb38d15c5c90b
ErikSalva <erikshalva@gmail.com>
2025-11-23 23:29:44 -0300

    style: corregir errores de indentación en authController

--- api-pedidos/src/controllers/authController.js
+++ api-pedidos/src/controllers/authController.js
@@ -5,22 +5,22 @@ exports.login = async (req, res) => {
     const { email, password } = req.body; // En OAuth2 estricto esto sería username y password
 
     if (!email || !password) {
-      return res.status(400).json({ 
-          error: 'invalid_request', 
-          error_description: 'Email y contraseña son requeridos' 
+      return res.status(400).json({
+        error: 'invalid_request',
+        error_description: 'Email y contraseña son requeridos'
       });
     }
 
     const resultado = await usuariosService.autenticarUsuario(email, password);
-    
+
     // OAuth2 espera status 200 OK con el json del token
     res.json(resultado);
 
   } catch (error) {
     // Si falla la contraseña, OAuth2 prefiere 'invalid_grant'
-    res.status(401).json({ 
-        error: 'invalid_grant', 
-        error_description: error.message 
+    res.status(401).json({
+      error: 'invalid_grant',
+      error_description: error.message
     });
   }
 };
